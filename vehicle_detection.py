30a686cd3ab26d530e605c188ea20fbedb2c6592
Nino Pereira <nino.pereira@dyson.com>
2017-02-02 11:06:35 +0000

    Bounding rectangles on detected cars

--- vehicle_detection.py
+++ vehicle_detection.py
@@ -218,10 +218,15 @@ def draw_rectangles(img,window_list,color= (255,255,255)):
     return labeled_img
 
 
-# In[41]:
+# In[60]:
 
 # Implement a sliding-window technique and use your trained classifier to search for vehicles in images.
-img = cv2.imread('test_images/test6.jpg')
+img = cv2.imread('test_images/test6.jpg') # two cars, black and white
+# img = cv2.imread('test_images/test5.jpg') # two cars, black and white
+# img = cv2.imread('test_images/test4.jpg') # two cars, black and white
+# img = cv2.imread('test_images/test3.jpg') # two cars, black and white
+# img = cv2.imread('test_images/test2.jpg') # no cars
+# img = cv2.imread('test_images/test1.jpg') # two cars, black and white
 # img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 height, width, channels = img.shape
 print('height, width, channels = ',height, width, channels)
@@ -257,7 +262,7 @@ plt.imshow(img)
 plt.show()
 
 
-# In[42]:
+# In[61]:
 
 # Create the heat map
 CV_FILLED = -1
@@ -306,7 +311,7 @@ plt.imshow(heat_map)
 plt.show()
 
 
-# In[43]:
+# In[62]:
 
 red_channel = heat_map[:,:,0]
 print("max:",np.amax(red_channel))
@@ -335,116 +340,47 @@ plt.show()
 # plt.show()
 
 
-# In[ ]:
+# In[69]:
 
-int main()
-{
-    cv::Mat input = cv::imread("../inputData/RotatedRect.png");
-
-    // convert to grayscale (you could load as grayscale instead)
-    cv::Mat gray;
-    cv::cvtColor(input,gray, CV_BGR2GRAY);
-
-    // compute mask (you could use a simple threshold if the image is always as good as the one you provided)
-    cv::Mat mask;
-    cv::threshold(gray, mask, 0, 255, CV_THRESH_BINARY_INV | CV_THRESH_OTSU);
-
-    // find contours (if always so easy to segment as your image, you could just add the black/rect pixels to a vector)
-    std::vector<std::vector<cv::Point>> contours;
-    std::vector<cv::Vec4i> hierarchy;
-    cv::findContours(mask,contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-
-    /// Draw contours and find biggest contour (if there are other contours in the image, we assume the biggest one is the desired rect)
-    // drawing here is only for demonstration!
-    int biggestContourIdx = -1;
-    float biggestContourArea = 0;
-    cv::Mat drawing = cv::Mat::zeros( mask.size(), CV_8UC3 );
-    for( int i = 0; i< contours.size(); i++ )
-    {
-        cv::Scalar color = cv::Scalar(0, 100, 0);
-        drawContours( drawing, contours, i, color, 1, 8, hierarchy, 0, cv::Point() );
-
-        float ctArea= cv::contourArea(contours[i]);
-        if(ctArea > biggestContourArea)
-        {
-            biggestContourArea = ctArea;
-            biggestContourIdx = i;
-        }
-    }
-
-    (/, if, no, contour, found)
-    if(biggestContourIdx < 0)
-    {
-        std::cout << "no contour found" << std::endl;
-        return 1;
-    }
-
-    (/, compute, the, rotated, bounding, rect, of, the, biggest, contour!, (this, is, the, part, that, does, what, you, want/need))
-    cv::RotatedRect boundingBox = cv::minAreaRect(contours[biggestContourIdx]);
-    (/, one, thing, to, remark:, this, will, compute, the, OUTER, boundary, box,, so, maybe, you, have, to, erode/dilate)
-    (/if, you, want, something, between, the, ragged, lines)
-
-
-
-    (/, draw, the, rotated, rect)
-    cv::Point2f corners[4];
-    boundingBox.points(corners);
-    cv::line(drawing, corners[0], corners[1], cv::Scalar(255,255,255));
-    cv::line(drawing, corners[1], corners[2], cv::Scalar(255,255,255));
-    cv::line(drawing, corners[2], corners[3], cv::Scalar(255,255,255));
-    cv::line(drawing, corners[3], corners[0], cv::Scalar(255,255,255));
-
-    (/, display)
-    cv::imshow("input", input);
-    cv::imshow("drawing", drawing);
-    cv::waitKey(0);
-
-    cv::imwrite("rotatedRect.png",drawing);
-
-    return 0;
-}
-
-
-# In[ ]:
-
-cv::findContours(mask,contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-float ctArea= cv::contourArea(contours[i]);
-
-for( int i = 0; i< contours.size(); i++ )
-    {
-        cv::Scalar color = cv::Scalar(0, 100, 0);
-        drawContours( drawing, contours, i, color, 1, 8, hierarchy, 0, cv::Point() );
-
-        float ctArea= cv::contourArea(contours[i]);
-        if(ctArea > biggestContourArea)
-        {
-            biggestContourArea = ctArea;
-            biggestContourIdx = i;
-        }
-    }
-cv::RotatedRect boundingBox = cv::minAreaRect(contours[biggestContourIdx]);
-                
-                    (/, draw, the, rotated, rect)
-    cv::Point2f corners[4];
-    boundingBox.points(corners);
-    cv::line(drawing, corners[0], corners[1], cv::Scalar(255,255,255));
-    cv::line(drawing, corners[1], corners[2], cv::Scalar(255,255,255));
-    cv::line(drawing, corners[2], corners[3], cv::Scalar(255,255,255));
-    cv::line(drawing, corners[3], corners[0], cv::Scalar(255,255,255));
-
-
-# In[112]:
-
+# define a threshold for minimum area required to be a positive detection
+area_th = 20
 imgray = heat_map[:,:,0]#cv2.cvtColor(heat_map,cv2.COLOR_BGR2GRAY)
 ret,thresh = cv2.threshold(imgray,60,255,0)
 im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-print(len(contours))
-print(contours)
-print(contours[:][0][1][0][1])
 
-cv2.drawContours(img, contours, -1, (0,255,0), 3)
+detected_car_pos = [];
+detected_car_rectangles = [];
+for contour in contours:
+    # get x and y values for all the contour points
+    area = cv2.contourArea(contour)
+    
+    if area>area_th:
+#         hull = cv2.convexHull(contour) # get the convex contour
+#         print("hull",hull)
+        x,y,w,h = cv2.boundingRect(contour)
+        pt1 = (x,y)
+        pt2 = (x+w,y+h)
+#         cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+#         x_values=[]
+#         y_values=[]
+#         for element in contour:
+#             x_values.append(element[0][0])
+#             y_values.append(element[0][1])
+
+        # define points for detected car in the form of a rectangle
+#         pt1 = (min(x_values),min(y_values))
+#         pt2 = (max(x_values),max(y_values))
+        detected_car_pos.append([(pt1[0]+pt1[1])/2,(pt1[0]+pt1[1])/2])
+        detected_car_rectangles.append([pt1,pt2])
+#     cv2.rectangle(im2,pt1, pt2, (255,255,255),thickness=4)
+
+im2 = draw_rectangles(img,detected_car_rectangles,color= (255,255,255))
+# cv2.drawContours(img, contours, -1, (0,255,0), 3)
+plt.imshow(heat_map)
+plt.show()
 plt.imshow(im2)
 plt.show()
+print("detected_car_pos",detected_car_pos)
 
 
 # In[ ]:
